8ee96ebd82e03ff47d5a06dab35ce08ce08b372d
Koren Feyhoa <kk9641288@gmail.com>
2025-02-15 01:02:31 +0700

    Миша, всё х&йня! Давай по новой

--- members/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Member
-
-class MemberSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Member
-        fields = ('id', 'name','surname', 'vk_id','login', 'password')
--- members/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Member
-from .serializers import MemberSerializer
-from rest_framework import generics
-
-class MemberListCreate(generics.ListCreateAPIView):
-    queryset = Member.objects.all()
-    serializer_class = MemberSerializer
