fa1f85429d045f8930c9db9518161b5ebf0ca349
Natan <shpilbergnatan@gmail.com>
2025-02-19 01:50:17 +0700

    Добавление роли организатора

--- MCIKIT/users/admin.py
+++ MCIKIT/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, UserRoles
 
 admin.site.register(User)
+admin.site.register(UserRoles)
 
 # Register your models here.
--- MCIKIT/users/models.py
+++ MCIKIT/users/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
 class User(models.Model):
+    Id = models.AutoField(primary_key=True)
     Name_User=models.CharField(max_length=30,help_text="ФИО")
     Password=models.CharField(max_length=30,help_text='Пароль')
     Login=models.CharField(max_length=20, help_text='Логин')
     VK_id=models.CharField(max_length=30, help_text='id ВК')
     Number_of_events_attended=models.IntegerField() #Доделать логику
-    Number_of_group=models.CharField(max_length=6, help_text="Номер группы")
+    Number_of_group=models.CharField(max_length=7, help_text="Номер группы")
     User_photo=models.ImageField(upload_to=None, height_field=None, width_field=None, blank=True)
     def __str__(self):
-        return self.Name_User, self.Number_of_events_attended, self.Number_of_group, self.VK_id
+        return f"{self.Name_User} | {self.Number_of_group}"
+
+class UserRoles(models.Model):
+    id = models.AutoField(primary_key=True)
+    uuid = models.ForeignKey(User, on_delete=models.CASCADE, help_text="Пользователь")
+    isOrganizer = models.BooleanField()
+
+    class Meta:
+        verbose_name = "User role"
+        ordering = ['id']
+
+    def __str__(self):
+        return f"<{self.id} | {self.uuid.Name_User} {self.uuid.Number_of_group}|" \
+                f"isOrganizer::{self.isOrganizer}"
