8d331fd9a9f0c479eb3b5620e502773d03c0f620
Natan <shpilbergnatan@gmail.com>
2025-02-21 15:31:05 +0700

    добавление моеделей в add_event и добавление event бд

--- MCIKIT/add_event/models.py
+++ MCIKIT/add_event/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Event(models.Model):
     RATE_CHOICES = (
@@ -11,6 +12,8 @@ class Event(models.Model):
     id = models.AutoField(primary_key=True)
     title_event = models.TextField(help_text="Название мероприятия")
     description_Event = models.TextField(help_text="Описание мероприятия")
-    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES) #доработать оценку
+    event_rate = models.PositiveSmallIntegerField(default = None, choices=RATE_CHOICES) #доработать оценку
+    event_place = models.TextField(default=None, help_text="Место проведеиня мероприятия")
+    event_date = models.DateTimeField(default=datetime.now,verbose_name="Дата проведения мероприятия")
     Event_photo = models.ImageField(upload_to=None, height_field=None, width_field=None)
 # Create your models here.
