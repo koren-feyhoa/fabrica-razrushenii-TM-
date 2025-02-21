9593610da87b04a977665f3ebb737cf17eeb7235
Koren-Feyhoa <kk9641288@gmail.com>
2025-02-21 20:00:06 +0300

    сделана переадресация после входа и выхода

--- MCIKIT/MCIKIT/settings.py
+++ MCIKIT/MCIKIT/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os.path
 from pathlib import Path
 
-from django.conf.global_settings import LOGIN_REDIRECT_URL
-from django.urls import reverse_lazy
+#from django.conf.global_settings import LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL='main'
 
 LOGOUT_REDIRECT_URL='start_page'
 
--- MCIKIT/users/urls.py
+++ MCIKIT/users/urls.py
@@ -1,13 +1,11 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from rest_framework.urls import app_name
-
 from . import views
+
 app_name="users"
 
 urlpatterns=[
 
     path('login/',views.LoginUser.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
