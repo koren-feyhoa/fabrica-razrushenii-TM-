09faead931511ae5f0ee52dd5632cf5ce6ca137c
Koren-Feyhoa <kk9641288@gmail.com>
2025-02-26 17:04:52 +0300

    модель мероприятия и рейтинга мероприятия

--- MCIKIT/add_event/admin.py
+++ MCIKIT/add_event/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
-from .models import Event
+from .models import Event, EventRating
+from django.db.models import Avg
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title_event', 'average_rating')
 
-admin.site.register(Event)
+    def average_rating(self, obj):
+        result = obj.ratings.aggregate(avg_rating=Avg('rating'))
+        return result['avg_rating'] or 0
+
+    average_rating.short_description = 'Средний рейтинг'
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(EventRating)
--- MCIKIT/add_event/models.py
+++ MCIKIT/add_event/models.py
@@ -1,7 +1,30 @@
+
 from django.db import models
-from datetime import datetime
+from datetime import datetime, time
+from django.conf import settings
+from django.db.models import ManyToManyField, Avg
+
 
 class Event(models.Model):
+
+    title_event = models.TextField(help_text="Название мероприятия")
+    description_Event = models.TextField(help_text="Описание мероприятия")
+    event_place = models.TextField(default=None, help_text="Место проведения мероприятия")
+    event_date = models.DateField(blank=False,verbose_name="Дата проведения мероприятия")
+    event_time=models.TimeField(blank=False, verbose_name="Время начала", default=time(12, 0))
+    Event_photo = models.ImageField(upload_to=None, height_field=None, width_field=None)
+    max_members=models.PositiveIntegerField(blank=True, verbose_name="Максимальное количество участников", default=0)
+    users_members=ManyToManyField(settings.AUTH_USER_MODEL,related_name='Участники')
+    prousers_members=ManyToManyField(settings.AUTH_USER_MODEL,related_name='Организаторы')
+
+    def __str__(self):
+        return self.title_event
+    def average_rate(self):
+        result = self.ratings.aggregate(avg_rating=Avg('rating'))
+        return result['avg_rating'] or 0
+
+
+class EventRating(models.Model):
     RATE_CHOICES = (
         (1, '1'),
         (2, '2'),
@@ -9,11 +32,13 @@ class Event(models.Model):
         (4, '4'),
         (5, '5'),
     )
-    id = models.AutoField(primary_key=True)
-    title_event = models.TextField(help_text="Название мероприятия")
-    description_Event = models.TextField(help_text="Описание мероприятия")
-    event_rate = models.PositiveSmallIntegerField(default = None, choices=RATE_CHOICES) #доработать оценку
-    event_place = models.TextField(default=None, help_text="Место проведеиня мероприятия")
-    event_date = models.DateTimeField(default=datetime.now,verbose_name="Дата проведения мероприятия")
-    Event_photo = models.ImageField(upload_to=None, height_field=None, width_field=None)
-# Create your models here.
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='event_ratings')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='ratings')
+    rating = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'event') # Один пользователь может оставить только одну оценку для мероприятия
+
+    def __str__(self):
+        return f"{self.user.Name_User} rated {self.event.title_event} with {self.rating}"
